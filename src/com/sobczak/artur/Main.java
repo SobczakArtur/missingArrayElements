52f4c80dd669d583d02fa0d6724f75cef7074c68
Artur Sobczak <sobczak.artur88@gmail.com>
2023-01-12 00:18:29 +0100

    Update Main.java

--- src/com/sobczak/artur/Main.java
+++ src/com/sobczak/artur/Main.java
@@ -4,11 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] tab = {1, 5, 10};
-        for (int arrayElements : tab) {
-            System.out.print(arrayElements + ", ");
+            int[] tab = {1, 5, 7, 11};
+
+            int missingElements = 0;
+            boolean elementFound;
+
+            for (int elementToFind = 0; elementToFind <= tab[3]; elementToFind++) {
+                elementFound = false;
+                    for (int elementInArray : tab) {
+                        if (elementToFind == elementInArray){
+                        elementFound = true;
+                    }
+                }
+                    if (!elementFound){
+                        missingElements = elementToFind;
+                        System.out.print(missingElements + " ");
+                    }
+            }
         }
-        System.out.println();
-        System.out.println("ffff");
     }
-}
